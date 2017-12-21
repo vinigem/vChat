7033c8b369b3b93410224381e0fe5cb95ac33717
Vinit Kumar <kumarv19@cat.com>
2017-12-21 18:11:07 +0530

    modified for heroku deployment

--- src/main/java/com/vini/Application.java
+++ src/main/java/com/vini/Application.java
@@ -3,8 +3,6 @@ package com.vini;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,13 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @SpringBootApplication
 @EnableAutoConfiguration
 @Controller
-public class Application extends SpringBootServletInitializer {
+public class Application {
 	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
