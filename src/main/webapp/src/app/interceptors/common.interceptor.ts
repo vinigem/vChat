a7105da96e0048a029045dfccd137afd40aa41bb
Vinit Kumar <kumarv19@cat.com>
2017-12-27 10:33:32 +0530

    added overlay

--- src/main/webapp/src/app/interceptors/common.interceptor.ts
+++ src/main/webapp/src/app/interceptors/common.interceptor.ts
@@ -1,32 +1,40 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/finally';
 
 import { OverlayService } from '../overlay/overlay.service';
 
 @Injectable()
 export class CommonInterceptor implements HttpInterceptor {
-        
-    constructor(private overlayService: OverlayService){}
+
+    constructor(private overlayService: OverlayService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // call to show overlay
         this.overlayService.showOverlay();
-        
+
         //  request start time
         let started = Date.now();
+
         return next
             .handle(request)
-            .do(event => {
-                if (event instanceof HttpResponse) {
-                    let elapsed = Date.now() - started;
-                    console.log(`Request for ${request.urlWithParams} took ${elapsed} ms.`);
-                    
-                    // call to hide overlay
-                    this.overlayService.hideOverlay();
+            .do((event: HttpEvent<any>) => { })
+            .catch(response => {
+                if (response instanceof HttpErrorResponse) {
+                    console.log('Processing http error', response);
                 }
-            }); 
+                return Observable.throw(response);
+            }).finally(() => {
+                let elapsed = Date.now() - started;
+                console.log(`Request for ${request.urlWithParams} took ${elapsed} ms.`);
+
+                // call to hide overlay
+                this.overlayService.hideOverlay();
+            });
     }
+
 }
